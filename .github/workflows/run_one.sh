d28bc47972db31c3f9b2a1bcf9d04550230f9217
xarantolus <xarantolus@protonmail.com>
2021-08-19 13:40:31 +0200

    Better GH Actions output

--- .github/workflows/run_one.sh
+++ .github/workflows/run_one.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
-echo "::group::$(git ls-files --full-name Makefile)"
+GROUP_NAME="$(git ls-files --full-name Makefile)"
+
+# The substitution strips "/Makefile" from the filepath
+echo "::group::${GROUP_NAME::-9}"
 make run
 echo "::endgroup::"
